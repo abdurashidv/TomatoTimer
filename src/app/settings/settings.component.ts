32783b539799c5ff7c7c7d188dec55657a97cba4
Abdurashid Vakhobov <abdurashid@confida.net>
2017-12-10 19:07:11 +0500

    Backbone for modal is ready

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-settings',
@@ -8,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
 
 export class SettingsComponent {
 
+  modalRef: BsModalRef;
   private items:string[] = ['FAQ', 'Settings', 'Tweet about us!'];
 
-  constructor() { }
-
+  constructor(private modalService: BsModalService) {}
+  
   showModal(value: string){
     console.log(value);
   }
 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 }
